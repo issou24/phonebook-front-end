1b8f2ebbef21eab3d9b76af3d38f0c8c4d5cb590
issou24 <127043948+issou24@users.noreply.github.com>
2025-07-23 01:23:56 +0200

    Update entryService.js

--- src/services/entryService.js
+++ src/services/entryService.js
@@ -3,23 +3,19 @@ import axios from "axios";
 const baseUrl = "https://part3-notes-backend-o51h.onrender.com/api/persons";
 
 const getAll = () => {
-  const data = axios.get(baseUrl);
-  return data.then((response) => response.data);
+  return axios.get(baseUrl).then(response => response.data);
 };
 
 const createPerson = (newObject) => {
-  const data = axios.post(baseUrl, newObject);
-  return data.then((response) => response.data);
+  return axios.post(baseUrl, newObject).then(response => response.data);
 };
 
 const updatePerson = (id, newObject) => {
-  const data = axios.put(`${baseUrl}/${id}`, newObject);
-  return data.then((response) => response.data);
+  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data);
 };
 
-const deletePerson = (id, obj) => {
-  const data = axios.delete(`${baseUrl}/${id}`, { data: obj });
-  return data.then((response) => response.data);
+const deletePerson = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
 };
 
 const entryService = {
